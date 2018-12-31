d72882888439fc9e2c39790cc6f05ab782907644
kvinaya <kvinaya@juniper.net>
2018-12-31 22:06:44 +0530

    modifing flask location sh

--- deployment/application_start.sh
+++ deployment/application_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export FLASK_APP=$(find "$(pwd -P)" -name "flaskerino.py")
-/usr/bin/flask run --host=0.0.0.0 >/dev/null 2>&1 &
+#/usr/bin/flask run --host=0.0.0.0 >/dev/null 2>&1 &
+python $FLASK_APP &
 
 sleep 10
