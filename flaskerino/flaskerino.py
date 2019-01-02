190fe8bb26ad9e65aad8b782cf624284977e7020
kvinaya <kvinaya@juniper.net>
2019-01-02 13:09:45 +0530

    modifing flask location sh

--- flaskerino/flaskerino.py
+++ flaskerino/flaskerino.py
@@ -47,7 +47,6 @@ def internal_error(error):
     return render_template('500.html', title="500 Internal Error"), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
-    #app.run(debug=True)
+    app.run(debug=True)
 
 
