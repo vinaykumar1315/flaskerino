3abe19dfdb6585d35fed2499206c56e489c5db03
kvinaya <kvinaya@juniper.net>
2019-01-02 13:00:29 +0530

    modifing flask location sh

--- deployment/validate.sh
+++ deployment/validate.sh
@@ -2,7 +2,7 @@
 status_code=$(curl --silent --head 0.0.0.0:5000/index | grep HTTP/ | awk -F ' ' '{ print $2 }')
 
 if [[ $status_code == "200" ]]; then
-     exit 0
+    exit 0
 else
     exit 1
 fi
